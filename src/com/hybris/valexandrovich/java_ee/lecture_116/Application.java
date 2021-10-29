775c3c8ba8abf7e85b1a439c90001c0a504282db
Valerii Bieloienko <valerii.bieloienko@otpbank.com.ua>
2021-10-29 16:52:28 +0300

    Lecture 117 - Periods

---
+++ src/com/hybris/valexandrovich/java_ee/lecture_116/Application.java
@@ -0,0 +1,54 @@
+package com.hybris.valexandrovich.java_ee.lecture_116;
+
+import com.hybris.valexandrovich.Logger;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.Month;
+import java.util.Calendar;
+import java.util.Date;
+
+public class Application {
+    public static void main(String[] args) {
+
+        Logger.addLine("------- " + Application.class.getName() + " -------");
+
+        LocalDate localDate = LocalDate.of(2030, Month.JANUARY, 10);
+        System.out.println(localDate);
+        Logger.addLine(String.valueOf(localDate));
+        localDate = localDate.plusDays(5).plusMonths(2);
+        System.out.println(localDate);
+        Logger.addLine(String.valueOf(localDate));
+
+        LocalTime localTime = LocalTime.of(10, 30);
+        System.out.println(localTime);
+        Logger.addLine(String.valueOf(localTime));
+        localTime = localTime.plusHours(2).plusMinutes(20);
+        System.out.println(localTime);
+        Logger.addLine(String.valueOf(localTime));
+
+        LocalDateTime localDateTime = LocalDateTime.of(localDate, localTime);
+        System.out.println(localDateTime);
+        Logger.addLine(String.valueOf(localDateTime));
+        localDateTime = localDateTime.minusHours(10).plusDays(2).plusWeeks(1);
+        System.out.println(localDateTime);
+        Logger.addLine(String.valueOf(localDateTime));
+
+        // before Java 8
+        Date myDate = new Date();
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(myDate);
+        calendar.set(Calendar.DATE, 1);
+        calendar.set(Calendar.MONTH, 1);
+        myDate = calendar.getTime();
+        System.out.println(myDate);
+        Logger.addLine(String.valueOf(myDate));
+
+        // exam tricks
+        LocalDate id = LocalDate.of(2010, Month.APRIL, 1);
+        id.plusDays(10);
+        System.out.println(id);
+        Logger.addLine(String.valueOf(id));
+    }
+}
---
+++ src/com/hybris/valexandrovich/java_ee/lecture_117/Application.java
@@ -0,0 +1,46 @@
+package com.hybris.valexandrovich.java_ee.lecture_117;
+
+import com.hybris.valexandrovich.Logger;
+
+import java.time.*;
+
+public class Application {
+    public static void main(String[] args) {
+
+        Logger.addLine("------- " + Application.class.getName() + " -------");
+
+        LocalDate start = LocalDate.of(2017, Month.JANUARY, 1);
+        LocalDate end = LocalDate.of(2017, Month.APRIL, 30);
+        Period period = Period.ofWeeks(1);
+
+        cleanAnimalCage(start, end);
+
+        System.out.println(start.plus(period));
+        Logger.addLine(String.valueOf(start.plus(period)));
+        System.out.println(LocalDateTime.now().plus(period));
+        Logger.addLine(String.valueOf(LocalDateTime.now().plus(period)));
+
+        Period yearAndMonth = Period.ofWeeks(2);
+        System.out.println(yearAndMonth);
+        Logger.addLine(String.valueOf(yearAndMonth));
+
+        cleanAnimalCage(start, end, period);
+
+    }
+
+    public static void cleanAnimalCage(LocalDate start, LocalDate end){
+        while (start.isBefore(end)){
+            System.out.println("Need to clean cage on date = " + start);
+            Logger.addLine("Need to clean cage on date = " + start);
+            start = start.plusMonths(1);
+        }
+    }
+
+    public static void cleanAnimalCage(LocalDate start, LocalDate end, Period period){
+        while (start.isBefore(end)){
+            System.out.println("Need to clean cage on date = " + start);
+            Logger.addLine("Need to clean cage on date = " + start);
+            start = start.plus(period);
+        }
+    }
+}
